c40638aed991d69032d61cbe8fc002cbd655808c
Roberto Efrain Hernandez <bh21346n@pace.edu>
2018-02-02 21:33:01 -0800

    Created FlickrClient file with Helper Methods for building URL

---
+++ Virtual-Tourist-Udacity/Model/FlickrClient.swift
@@ -0,0 +1,69 @@
+//
+//  FlickrClient.swift
+//  Virtual-Tourist-Udacity
+//
+//  Created by Roberto Hernandez on 2/2/18.
+//  Copyright © 2018 Roberto Efrain Hernandez. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+// MARK: -- Flickr Constants
+/***************************************************************/
+
+class FlickrClient: NSObject {
+    
+    static let sharedInstance = FlickrClient()
+    
+    
+    // MARK: -- Helper for Creating a URL from Parameters
+    /***************************************************************/
+    private func flickrURLFromParameters(_ parameters: [String:String]) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = Constants.Flickr.APIScheme
+        components.host = Constants.Flickr.APIHost
+        components.path = Constants.Flickr.APIPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        return components.url!
+    }
+    
+    /* given a dictionary of parameters, covert unsafe ASCII characters and correctly formated url string. */
+    func escapedParameters(_ parameters: [String:AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                // make sure that it is a string value
+                let stringValue = "\(value)"
+                
+                // escape it
+                let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                
+                // append it
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+                
+            }
+            
+            return "?\(keyValuePairs.joined(separator: "&"))"
+        }
+    }
+    
+    func bboxString(longitude:Double, latitude:Double) -> String {
+        let minimumLon = max(longitude - Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.0)
+        let minimumLat = max(latitude - Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.0)
+        let maximumLon = min(longitude + Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.1)
+        let maximumLat = min(latitude + Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.1)
+        return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
+    }
+}
