05d6208c41a8aae7d48da6ae628df36e4a783eaa
Roberto Efrain Hernandez <bh21346n@pace.edu>
2018-02-06 23:15:26 -0800

    Installed CollectionViewSlantedLayout Cocoapod. Set up custom collection view cell to include CollectionViewSlantedCell subclass. Almost finished setting up TouristMapViewController. Did some prep code by setting up extensions for necessary code for PhotoAlbumViewController.

--- Virtual-Tourist-Udacity/Controller/PhotoAlbumViewController.swift
+++ Virtual-Tourist-Udacity/Controller/PhotoAlbumViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import CoreData
+import MapKit
+import CollectionViewSlantedLayout
+
+// MARK: -- Photo Album View Controller
+/***************************************************************/
+
+private let reuseIdentifier = "PhotoAlbumCell"
 
 class PhotoAlbumViewController: UIViewController {
-
+    
+    /* Outlets */
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var flickrCollectionView: UICollectionView!
+    @IBOutlet weak var collectionViewLayout: CollectionViewSlantedLayout!
+    @IBOutlet weak var newCollectionButton: UIButton!
+    
+    /* Properties */
+    private let persistanContainter = NSPersistentContainer(name: "Photo")
+    var selectedPin : Pin!
+    var photoData : [Photo] = [Photo]()
+    var selectedIndexPaths = [NSIndexPath]()
+    var photosSelected = false
+    var currentPage = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        /* Make First and Last cell not slanted when the view loads */
+        collectionViewLayout.isFirstCellExcluded = true
+        collectionViewLayout.isLastCellExcluded = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +45,44 @@ class PhotoAlbumViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: -- MapView Delegate Methods
+/***************************************************************/
+extension PhotoAlbumViewController: MKMapViewDelegate {
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: -- Fetch Results Delegate Methods
+/***************************************************************/
+extension PhotoAlbumViewController: NSFetchedResultsControllerDelegate {
+    
+}
+
+// MARK: -- CollectionView Data Source Methods
+/***************************************************************/
+extension PhotoAlbumViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoData.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let flickrCell = collectionView.dequeueReusableCell(withReuseIdentifier: "flickrPhotoCell", for: indexPath) as! PhotoAlbumCollectionViewCell
+        
+        return flickrCell
+    }
+}
 
+// MARK: -- CollectionView Slanted Layout Delegate Methods
+/***************************************************************/
+extension PhotoAlbumViewController: CollectionViewDelegateSlantedLayout {
+    
+}
+
+// MARK: -- UIImageView Extention
+/***************************************************************/
+extension UIImageView {
+    
 }
--- Virtual-Tourist-Udacity/Controller/TouristMapViewController.swift
+++ Virtual-Tourist-Udacity/Controller/TouristMapViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
-import ChameleonFramework
+import CoreData
+import MapKit
+
+// MARK: -- Tourist Map View Controller
+/***************************************************************/
 
 class TouristMapViewController: UIViewController {
-
+    
+    /* Outlets */
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var editLabel: UILabel!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    /* Properties */
+    var editLabelVisible = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        editLabel.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +34,101 @@ class TouristMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: -- Navigation
+    /***************************************************************/
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowPhotoAlbumVC" {
+            let controller = segue.destination as! PhotoAlbumViewController
+            let selectedPin = sender as! Pin
+            controller.selectedPin = selectedPin
+        }
+    }
 }
 
+// MARK: -- Map View Delegate Methods
+/***************************************************************/
+extension TouristMapViewController: MKMapViewDelegate {
+    
+    func loadAnnotations() {
+        let fetchRequest : NSFetchRequest<Pin> = Pin.fetchRequest()
+        
+        do {
+            let searchResults = try CoreDataStack.getContext().fetch(fetchRequest)
+            print("There are \(searchResults.count) locations")
+            var annotations = [MKPointAnnotation]()
+            for result in searchResults as [Pin] {
+                
+                let lat = CLLocationDegrees(result.latitude)
+                let long = CLLocationDegrees(result.longitude)
+                let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = coordinate
+                annotations.append(annotation)
+                
+            }
+            performUIUpdatesOnMain {
+                self.mapView.addAnnotations(annotations)
+                print("Annotations added to the map view.")
+            }
+        }
+        catch {
+            print("Error fetching annotations: \(error)")
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        mapView.deselectAnnotation(view.annotation, animated: true)
+        let lat = view.annotation?.coordinate.latitude
+        let lon = view.annotation?.coordinate.longitude
+        print("The selected pin's lat:\(String(describing: lat)), lon:\(String(describing: lon))")
+        let fetchRequest:NSFetchRequest<Pin> = Pin.fetchRequest()
+        do {
+            let searchResults = try CoreDataStack.getContext().fetch(fetchRequest)
+            for pin in searchResults as [Pin] {
+                if pin.latitude == lat!, pin.longitude == lon! {
+                    let selectedPin = pin
+                    print("Found pin info.")
+                    if editLabelVisible {
+                        /* Delete selectedPin from CoreData */
+                        performUIUpdatesOnMain {
+                            
+                            CoreDataStack.getContext().delete(selectedPin)
+                            CoreDataStack.saveContext()
+                            self.mapView.removeAnnotation(view.annotation!)
+                            print("Deleted the selected pin.")
+                            
+                        }
+                    } else {
+                        /* Segue to Photo Album VC */
+                        print("Perform segue to the photo album.")
+                        performUIUpdatesOnMain {
+                            self.performSegue(withIdentifier: "ShowPhotoAlbumVC", sender: selectedPin)
+                        }
+                    }
+                }
+            }
+        }catch {
+            print("Error: \(error)")
+        }
+    }
+}
